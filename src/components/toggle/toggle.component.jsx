903a2df70b8b0937e6497507ec0943fb0a36236f
pbcarloz <pb_carlos@outlook.com>
2023-02-22 16:45:50 +0900

    added git pages

--- src/components/toggle/toggle.component.jsx
+++ src/components/toggle/toggle.component.jsx
@@ -2,15 +2,13 @@ import React, { useContext } from 'react';
 import './toggle.style.css'
 import Sun from '../../img/icons8-light-mode-78.png'
 import Moon from '../../img/icons8-dark-mode-48.png'
-import { ThemeContext, LangContext } from '../../context';
+import { ThemeContext } from '../../context';
 
 const Toggle = () => {
     const theme = useContext(ThemeContext);
-    const lang = useContext(LangContext);
 
     const handleClick = () => {
         theme.dispatch({ type: "TOGGLE" })
-        lang.dispatch({ type: "JAP" })
     }
 
 
