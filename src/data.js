1e9b7dbb5a97765c146b56e008215f1edbb8d9fc
pbcarloz <pb_carlos@outlook.com>
2023-03-03 17:08:29 +0900

    Added new project and fixed css

--- src/data.js
+++ src/data.js
@@ -6,15 +6,15 @@ export const products = [
         img: "https://raw.githubusercontent.com/pbcarloz/react-coderhouse/main/public/Mainpage.PNG",
         link: "https://rey-store.netlify.app",
         github: "https://github.com/pbcarloz/Rey-Store",
-        desc: "A single page Web App i made from scratch using React, State, Context, Router, as well as Firebase to log in and save data with a Google Accounty. Actually in the process to update the code to integrate styled components, React Redux and Typescript. To finally implement the payment function with Stripe ",
+        desc: "An Ecommerce App i made based on an old school project using React, State, Context, Router, as well as Firebase to log in and save data with a Google Accounty. Actually in the process to update the code to integrate styled components, React Redux and Typescript. To finally implement the payment function with Stripe ",
 
     },
     {
         id: 2,
-        img: "https://raw.githubusercontent.com/pbcarloz/Cat-Directory/master/src/media.PNG",
-        link: "https://pbcarloz.github.io/Cat-Directory/",
-        github: "https://github.com/pbcarloz/Cat-Directory",
-        desc: "The first React App i made using State Management to show the render Behaviour in the form of a contact directory allowing to search through a Search Box",
+        img: "https://iili.io/HMP4msp.png",
+        link: "https://pbcarloz.github.io/NekoTypes/",
+        github: "https://github.com/pbcarloz/NekoTypes",
+        desc: "One of the first Typescript projects i made using React State Management that renders the result that match the text typed in the searchbox",
 
     },
     {
@@ -25,20 +25,29 @@ export const products = [
         desc: "A web page i made using Pure HTML, CSS and Javascript to be used as a landing page for a medic to facilitate the communication, evaluate and create appointments through EmailJS.",
 
     },
+
     {
-        id: 4,
+        id: 5,
         img: "https://serving.photos.photobox.com/773061184ff603ad99130bb3ccbf02b4870d0ec386b936e54bff14b5a2217f711fadd5ff.jpg",
         link: "https://pbcarloz.github.io/Gradient-Generator/",
         github: "https://github.com/pbcarloz/Gradient-Generator",
-        desc: "The first web page i made back in 2020 to generate CSS gradient Value using HTML CSS and to show DOM manipulation with Javascript",
+        desc: "The first web page i made back in March 2020 to generate CSS gradient Value using HTML CSS and to show DOM manipulation with Javascript",
 
     },
     {
-        id: 5,
-        img: "https://iili.io/HMP4msp.png",
-        link: "https://pbcarloz.github.io/NekoTypes/",
-        github: "https://github.com/pbcarloz/NekoTypes",
-        desc: "One of the first Typescript projects i made based from my React directory App that renders the result that match the text typed in the searchbox",
+        id: 6,
+        img: "https://i.ibb.co/JQCXMQ7/Landing-Page.png",
+        link: "https://pbcarloz.github.io/LandingPage/",
+        github: "https://github.com/pbcarloz/LandingPage",
+        desc: "One of the first pages i made from scratch using pure HTML CSS and Javascript",
+
+    },
+    {
+        id: 7,
+        img: "https://raw.githubusercontent.com/pbcarloz/react-coderhouse/main/public/Mainpage.PNG",
+        link: "https://coder-house-rey-clothing.netlify.app",
+        github: "https://github.com/pbcarloz/react-coderhouse",
+        desc: "First version of an E-commerce i developed using React with functional components. Several hooks were used, such as useState, useContext, useNavigation, react router, etc. All integrated with Google firebase to make requests for data collections, order storage, users, etc.",
 
     },
 ];
